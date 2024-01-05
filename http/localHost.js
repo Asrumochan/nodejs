7b52f26fce45f04808321bc23d931162f2c7a30b
Asrumochan <asrumochanparida99@gmail.com>
2024-01-05 22:52:53 +0530

    sdf

--- http/localHost.js
+++ http/localHost.js
@@ -1,10 +1,13 @@
 //const http =require('http');
 import http from 'http'
+import dotenv from 'dotenv'
+dotenv.config({path:'./config/.config'})
+let port = process.env.PORT
 const server =http.createServer((req,resp)=>{
     resp.end("Hello , Http server,,,,...............................")
 })
 
-server.listen(8000,(err)=>{
+server.listen(port,(err)=>{
     if(err) throw err
-    console.log("Server is running.......................................'")
+    console.log(`Server is running on ${port}`)
 })
