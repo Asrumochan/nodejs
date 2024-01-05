c70255463f0e0d0eb58e73b089cd4ad53552c43f
Asrumochan <asrumochanparida99@gmail.com>
2024-01-05 22:43:06 +0530

    ghcghc

--- http/staticWeb.js
+++ http/staticWeb.js
@@ -1,15 +1,27 @@
-import http from "http";
-import fs from "fs"
-let server =http.createServer((req,resp)=>{
-    if(req.url ==="/index"){
-        fs.readFile('views/index.html','utf-8',(err,data)=>{
-            if(err) throw err 
-            resp.end(data)
-        })
-       }
-})
+import fs from 'fs'
+import http from 'http'
 
+let server = http.createServer((req,resp)=>{
+   if(req.url ==="/index"){
+    fs.readFile('views/index.html','utf-8',(err,data)=>{
+        if(err) throw err 
+        resp.end(data)
+    })
+   }
+    if(req.url ==="/about"){
+    fs.readFile('views/about.html','utf-8',(err,data)=>{
+        if(err) throw err 
+        resp.end(data)
+    })
+   }
+    if(req.url ==="/service"){
+    fs.readFile('views/services.html','utf-8',(err,data)=>{
+        if(err) throw err 
+        resp.end(data)
+    })
+   }
+})
 server.listen(8080,(err)=>{
-    if(err) throw err;
-    console.log(`server is running on ${8080} port`)
+    if(err) throw err 
+    console.log("Server is Running")
 })
