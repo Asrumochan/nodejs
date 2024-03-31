56adfdb5cb3482f273678f4b73853fb6f0192f52
Asrumochan <asrumochanparida99@gmail.com>
2024-03-31 13:04:50 +0530

    sdf

--- http/jurl.js
+++ http/jurl.js
@@ -3,7 +3,7 @@ import http from 'http'
 
 let server = http.createServer((req,res)=>{
     let reqUrl = url.parse(req.url,true)
-
+    console.log(reqUrl)
     console.log("welcome to server !");
     res.end(`HII ${reqUrl.query.name} welcome to my server!`)
 })
