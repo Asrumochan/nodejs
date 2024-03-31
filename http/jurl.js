e30fb28651bbca8e0e96a7d4c0c3dfb5c9e58a34
Asrumochan <asrumochanparida99@gmail.com>
2024-03-31 12:44:11 +0530

    addf

---
+++ http/jurl.js
@@ -0,0 +1,11 @@
+import url from 'url'
+import http from 'http'
+
+let server = http.createServer((req,res)=>{
+    let reqUrl = url.parse(req.url,true)
+
+    console.log("welcome to server !");
+    res.end(`HII ${reqUrl.query.name} welcome to my server!`)
+})
+
+server.listen(8000,(err)=>{})
