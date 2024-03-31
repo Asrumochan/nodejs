89bea47ab9ba894db22bfc1794e1c60fd5c1fd77
Asrumochan <asrumochanparida99@gmail.com>
2024-03-31 13:06:10 +0530

    asd

--- http/jurl.js
+++ http/jurl.js
@@ -5,7 +5,7 @@ let server = http.createServer((req,res)=>{
     let reqUrl = url.parse(req.url,true)
     console.log(reqUrl)
     console.log("welcome to server !");
-    res.end(`HII ${reqUrl.query.name} welcome to my server!`)
+    res.end(`HII ${reqUrl.query.name} with id=${reqUrl.query.id} welcome to my server!`)
 })
 
 server.listen(8000,(err)=>{})
