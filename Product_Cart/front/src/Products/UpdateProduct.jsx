f12f0699f3bbcef78484477aad305358fe058926
Asrumochan <asrumochanparida99@gmail.com>
2024-04-05 13:36:23 +0530

    GDH

--- Product_Cart/front/src/Products/UpdateProduct.jsx
+++ Product_Cart/front/src/Products/UpdateProduct.jsx
@@ -7,8 +7,6 @@ const UpdateProduct = () => {
   let navigate=useNavigate();
   let id=location.state;
   const [product,setProduct]=useState({})
-  const [updateProd,setUpdateProd]=useState({name:"",image:"a",price:0,qty:0,info:""})
- 
   const [imageString, setImageString] = useState('');
   const formRef = useRef(null);
 
@@ -17,9 +15,9 @@ const UpdateProduct = () => {
     .then((resp)=>{
       setProduct(resp.data)
     })
-  })
+  },[])
   const updateHandler=(evt)=>{
-    setUpdateProd({...updateProd,[evt.target.name]:evt.target.value})
+    setProduct({...product,[evt.target.name]:evt.target.value})
 }
 const handleImageChange = (event) => { // 5MB (adjust as needed)
   const file = event.target.files[0];
@@ -29,13 +27,13 @@ const handleImageChange = (event) => { // 5MB (adjust as needed)
     const imageData = event.target.result;
     setImageString(imageData);
     console.log(imageString)
-    setUpdateProd({...updateProd,image:imageString})
+    setProduct({...product,image:imageString})
   };
   
 };
 const addProduct=(evt)=>{
   evt.preventDefault();
-  Axios.put(`http://127.0.0.1:5000/api/products/${id}`,updateProd)
+  Axios.put(`http://127.0.0.1:5000/api/products/${id}`,product)
   .then((resp)=>{
     console.log("product updated")
     navigate('/admin')
@@ -53,29 +51,29 @@ const addProduct=(evt)=>{
         <div className="card">
           <div className="card-header">
             <h1>Update Product</h1>
-      <pre>{JSON.stringify(updateProd)}</pre>
+      <pre>{JSON.stringify(product)}</pre>
           </div>
           <div className="card-body ">
             <form className='form' onSubmit={addProduct} ref={formRef} >
               <div className="form-group mt-4 border border-primary">
                 <label>Enter Name</label>
-              <input className='form-control' required type="text" placeholder={product.name}  onChange={updateHandler} name="name"/>
+              <input className='form-control' required type="text" value={product.name}  onChange={updateHandler} name="name"/>
               </div>
               <div className="form-group mt-4 border border-primary">
               <label>Upload Image</label>
-              <input className='form-control' required type="file"   onChange={handleImageChange} name="image"/>
+              <input className='form-control'  type="file"   onChange={handleImageChange} name="image"/>
               </div>
               <div className="form-group mt-4 border border-primary">
               <label>Enter Price</label>
-              <input className='form-control' required type="number" placeholder={product.price} onChange={updateHandler} name="price" />
+              <input className='form-control' required type="number" value={product.price} onChange={updateHandler} name="price" />
               </div>
               <div className="form-group mt-4 border border-primary">
               <label>Enter Quantity</label>
-              <input className='form-control' required type="number" placeholder={product.qty} onChange={updateHandler} name="qty"/>
+              <input className='form-control' required type="number" value={product.qty} onChange={updateHandler} name="qty"/>
               </div>
               <div className="form-group mt-4 border border-primary">
               <label>Enter Info</label>
-              <input className='form-control' required type="text" placeholder={product.info} onChange={updateHandler} name="info"/>
+              <input className='form-control' required type="text" value={product.info} onChange={updateHandler} name="info"/>
               </div>
               <div className="form-group mt-4">
                 <input className='form-control btn btn-primary' type="submit" value='Add Product' />
