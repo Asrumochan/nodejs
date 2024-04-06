5e902df95eebdb4dd2ba786ebeea3d9461ee3bc4
Asrumochan <asrumochanparida99@gmail.com>
2024-04-06 21:55:47 +0530

    sd

--- Product_Cart/front/src/Products/cart.jsx
+++ Product_Cart/front/src/Products/cart.jsx
@@ -4,14 +4,25 @@ import {useLocation} from 'react-router-dom'
 
 const Cart = () => {
   const location = useLocation();
-  const [products,setProducts]=useState([{}])
+  const [products,setProducts]=useState([])
+  const [totalPrice, setTotalPrice] = useState(0);
   useEffect(()=>{
     Axios.get(`http://127.0.0.1:5000/api/products/${location.state}`)
     .then((res)=>{
       setProducts([res.data])
-    })
+     })
     .catch()
   },[])
+  useEffect(() => {
+    const calculateTotalPrice = () => {
+      const total = products.reduce((acc, product) => {
+        return acc + product.price * product.qty;
+      }, 0);
+      setTotalPrice(total);
+    };
+
+    calculateTotalPrice();
+  }, [products]);
   return (
     <div className='container mt-5'>
       <div className="row">
@@ -20,6 +31,7 @@ const Cart = () => {
             <thead>
               <tr>
                 <th>Name</th>
+                <th>Image</th>
                 <th>Price</th>
                 <th>Quantity</th>
                 <th>Total_Price</th>
@@ -30,6 +42,7 @@ const Cart = () => {
                 products.map((product)=>{
                   return <tr key={product._id}>
                           <td>{product.name}</td>
+                          <td><img src={product.image} height='50px' alt="" /></td>
                           <td>{product.price}</td>
                           <td>{product.qty}</td>
                           <td>{product.price*product.qty}</td>
@@ -40,6 +53,7 @@ const Cart = () => {
         </table>
         </div>
       </div>
+        {totalPrice}
     </div>
   )
 }
