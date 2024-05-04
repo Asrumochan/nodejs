13ecd1c2586cdbca99750446cc929a629fae8a7a
Asrumochan <asrumochanparida99@gmail.com>
2024-05-04 23:53:07 +0530

    fgh

--- express/project-01/index.js
+++ express/project-01/index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json())
 mongoose.connect('mongodb://127.0.0.1:27017/asru')
 .then(()=>{console.log("MongDB connected")})
 .catch((err)=>{console.log(err)
-    proces s.exit(1)})
+    process.exit(1)})
 const userSchema = new mongoose.Schema({
     name:{ 
         type:String,
