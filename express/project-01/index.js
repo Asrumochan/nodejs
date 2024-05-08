b1141d61a4ac5cea3b92f42ae6c491fc3ebb479a
Asrumochan <asrumochanparida99@gmail.com>
2024-05-08 23:27:02 +0530

    gh

--- express/project-01/index.js
+++ express/project-01/index.js
@@ -2,7 +2,7 @@ import express from 'expres'
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
-let app=express();
+let app= nexpress();
 app.use(bodyParser.json())
 mongoose.connect('mongodb://127.0.0.1:27017/asru')
 .then(()=>{console.log("MongDB connected")})
